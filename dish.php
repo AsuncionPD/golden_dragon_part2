e14b5e11d1c56c4d9f61296412bdadd466d792af
Asunción Peña <asuncion.pena@ucr.ac.cr>
2023-11-12 03:22:08 -0600

    Create php files

---
+++ dish.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dish Information</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap"rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mooli&display=swap" rel="stylesheet">
+    <!-- google fonts -->
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <header>
+        <div class="top-page">
+            <div class="img-logo" ></div>
+    </header>
+    <main class="main-content">
+        <div class="dish-container">
+            <img class="img-dish" src="imgs/gold-cup.png" alt="Gold cup">
+            <div class="dish-information">
+                <h2 class="dish-name">Gold-cup</h2>
+                <p class="description">Shrimp and Broccoli with a brown sauce with garlic.</p>
+                <button class="star-btn"><img class="img-star" src="./imgs/empty-star.png" alt="empty-star"></button>
+                <div class="dish-caracteristics">
+                    <div>
+                        <h3 class="margin-details">Category</h3>
+                        <p class="margin-text">Starter</p>
+                    </div>
+                    <div class="divider"></div>
+                    <div>
+                        <h3 class="margin-details">Dish for</h3>
+                        <p class="margin-text">2</p>
+                    </div>
+                    <div class="divider"></div>
+                    <div>
+                        <h3 class="margin-details">Price</h3>
+                        <p class="margin-text">$15</p>
+                    </div>
+                </div>
+                <h3>Related dishes</h3>
+                <div class="imgs-related-container">
+                    <img class="img-related" src="./imgs/pork-chive.png" alt="pork-chive">
+                    <img class="img-related" src="./imgs/spring-rolls.png" alt="spring-rolls">
+                    <img class="img-related" src="./imgs/cucumber-salad.png" alt="cucumber-salad">
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="footer-content"></div>
+    </footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Golden Drangon</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mooli&display=swap" rel="stylesheet">
+    <!-- google fonts -->
+    <link rel="stylesheet" href="./css/main.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="top-page">
+            <div class="img-logo" ></div>
+        </div>
+        <nav class="top-nav">
+            <!--movile nav btn-->
+            <input class="mobile-check" type="checkbox">
+            <label class="mobile-btn">
+                <span>
+
+                </span>
+            </label>
+            <!--movile nav btn-->
+
+            <ul class="nav-list">
+                <li><a class="nav-list-link" href="#">Home</a></li>
+                <li><a class="nav-list-link" href="#">About</a></li>
+                <li><a class="nav-list-link" href="#">Menu</a></li>
+                <li><a class="nav-list-link" href="#">Contact</a></li>
+                <li><a class="nav-list-link" href="#">Login</a></li>
+            </ul>
+        </nav>
+        <section class="hero-container home-page-bg">
+            <h1 class="hero-title">The authentic flavors of China</h1>
+            <p class="hero-subtitle">Every dish is a journey to the heart of the Middle Kingdom</p>
+        </section>
+    </header>
+
+    <main>
+        <!--options-->
+        <section class="container">
+            <div class="categories-container">
+                <button class="category">
+                    <img class="vector-img" src="imgs/vector-starters.png" alt="vector-starters">
+                    <p class="font-options">Starters</p>
+                </button>
+
+                <button class="category">
+                    <img class="vector-img" src="imgs/vector-main_course.png" alt="vector-main_course">
+                    <p class="font-options">Main Courses</p>
+                </button>
+
+                <button class="category">
+                    <img class="vector-img" src="imgs/vector-desserts.png" alt="vector-desserts">
+                    <p class="font-options">Desserts</p>
+                </button>
+
+                <button class="category">
+                    <img class="vector-img" src="imgs/vector-drinks.png" alt="vector-drinks">
+                    <p class="font-options">Drinks</p>
+                </button>
+            </div>
+        </section>
+        <!--options-->
+
+        <!--space-->
+        <img class="img-in-the-middle" src="imgs/space-bg.png" alt="secong_img-home_page">
+        <!--space-->
+
+        <!--menu-->
+        <section class="container">
+            <h1 class="featured-dishes-title">Featured dishes</h1>
+
+            <div class="dishes-container">
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/gold-cup.png" alt="Gold cup">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$15</h2>
+                        <h3 class="dish-title">Gold Cup</h3>
+                        <p class="dish-description">Shrimp and Broccoli with a brown sauce with garlic.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/kung-pao-chicken.png" alt="Kung Pao Chicken">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$25</h2>
+                        <h3 class="dish-title">Kung Pao Chicken</h3>
+                        <p class="dish-description">Cubed chicken in a sweet, spicy, and savory sauce.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/orange-chicken.png" alt="Orange Chicken">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$18</h2>
+                        <h3 class="dish-title">Orange Chicken</h3>
+                        <p class="dish-description">Pan-fried, and coated chicken in orange sauce.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/pork-chive.png" alt="Pork and chive">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$12</h2>
+                        <h3 class="dish-title">Chive Dumplings</h3>
+                        <p class="dish-description">Juicy dumplings filled with ground pork and chives.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+            </div>
+
+            <div class="dishes-container">
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/spring-rolls.png" alt="Spring Rolls">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$12</h2>
+                        <h3 class="dish-title">Spring Rolls</h3>
+                        <p class="dish-description">Thin pastry wraps filled with vegetables and a type of meat</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/almond-jelly.png" alt="Almond Jelly">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$10</h2>
+                        <h3 class="dish-title">Almond Jelly</h3>
+                        <p class="dish-description">Gelatin and fruit salad swimming in a sweet syrup.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/cucumber-salad.png" alt="Cucumber Salad">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$12</h2>
+                        <h3 class="dish-title">Pai Huang Gua</h3>
+                        <p class="dish-description">Cucumbers in a vinegar-based with garlic and chilies.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/liangpi.png" alt="Liangpi">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$22</h2>
+                        <h3 class="dish-title">Liangpi</h3>
+                        <p class="dish-description">Cold wheat noodles with a savory sauce.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+            </div>
+
+            <div class="dishes-container container-width">
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/corn-soup.png" alt="Corn Soup">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$20</h2>
+                        <h3 class="dish-title">Corn Chicken Soup</h3>
+                        <p class="dish-description">Sweet corn and ground chicken.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+
+                <section class="dish">
+                    <div class="img-container">
+                        <img class="dish-img" src="imgs/grilled-chicken.png" alt="Grilled Chicken">
+                    </div>
+                    <section class="padding">
+                        <h2 class="price dish-title">$25</h2>
+                        <h3 class="dish-title">Grilled Chicken</h3>
+                        <p class="dish-description">Chicken with a sweet-savory glaze.</p>
+                        <a class="btn" href="#">See more</a>
+                    </section>
+                </section>
+            </div>
+        </section>
+    </main>
+
+    <!--footer-->
+    <footer class="footer">
+        <div class="footer-content">
+            <section>
+                <h3>Golden Drangon</h3>
+                <p>We are dedicated to satisfying the gastronomic needs of our customers,
+                    preparing and offering food and services of the highest quality and personalized
+                    attention that ensures their satisfaction</p>
+            </section>
+            <div class="footer-links">
+                <section>
+                    <h3>Get to Know Us</h3>
+                    <ul class="nav-bottom-list">
+                        <li><a class="nav-bottom-link" href="#">About Us</a></li>
+                        <li><a class="nav-bottom-link" href="#">Rules & Reservation Policies</a></li>
+                        <li><a class="nav-bottom-link" href="#">Accessibility</a></li>
+                        <li><a class="nav-bottom-link" href="#">Media Center</a></li>
+                        <li><a class="nav-bottom-link" href="#">Site Map</a></li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3>Let Us Help You</h3>
+                    <ul class="nav-bottom-list">
+                        <li><a class="nav-bottom-link" href="#">Your Account</a></li>
+                        <li><a class="nav-bottom-link" href="#">Your Reservations</a></li>
+                        <li><a class="nav-bottom-link" href="#">Contact Us</a></li>
+                        <li><a class="nav-bottom-link" href="#">Help Center</a></li>
+                        <li><a class="nav-bottom-link" href="#">Submit Feedback</a></li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3>Follow Us</h3>
+                    <ul class="nav-bottom-list">
+                        <li><a class="nav-bottom-link" href="#">Instagram</a></li>
+                        <li><a class="nav-bottom-link" href="#">Facebook</a></li>
+                    </ul>
+                </section>
+
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
