81095b61de8ca19d73cd14eaa5bc706a6845497b
@Naho_Lawrence <daleyshka.lawrence@ucr.ac.cr>
2023-11-26 19:06:24 -0600

    Convert the Dish Detail Page to PHP

--- dish.php
+++ dish.php
@@ -1,3 +1,31 @@
+<?php
+require_once './backend/database.php';
+
+if ($_GET) {
+    $item = $database->select("tb_dishes", [
+        "[>]tb_category_number_people" => ["id_category_people" => "id_category_people"],
+        "[>]tb_dish_category" => ["id_dish_category" => "id_dish_category"]
+    ], [
+        "tb_dishes.id_dish",
+        "tb_dishes.dish_name",
+        "tb_dishes.dish_name_chi",
+        "tb_dishes.dish_description",
+        "tb_dishes.dish_description_chi",
+        "tb_dishes.dish_image",
+        "tb_dishes.dish_price",
+        "tb_category_number_people.id_category_people",
+        "tb_category_number_people.category_people_name",
+        "tb_category_number_people.category_people_description",
+        "tb_dish_category.id_dish_category",
+        "tb_dish_category.dish_category_name",
+        "tb_dish_category.dish_category_description",
+    ], [
+        "id_dish" => $_GET["id"]
+    ]);
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,36 +47,45 @@
         <div class="top-page">
             <div class="img-logo" ></div>
     </header>
-    <main class="main-content">
+    <main class="main-content">   
         <div class="dish-container">
-            <img class="img-dish" src="imgs/gold-cup.png" alt="Gold cup">
-            <div class="dish-information">
-                <h2 class="dish-name">Gold-cup</h2>
-                <p class="description">Shrimp and Broccoli with a brown sauce with garlic.</p>
-                <button class="star-btn"><img class="img-star" src="./imgs/empty-star.png" alt="empty-star"></button>
-                <div class="dish-caracteristics">
-                    <div>
-                        <h3 class="margin-details">Category</h3>
-                        <p class="margin-text">Starter</p>
-                    </div>
-                    <div class="divider"></div>
-                    <div>
-                        <h3 class="margin-details">Dish for</h3>
-                        <p class="margin-text">2</p>
-                    </div>
-                    <div class="divider"></div>
-                    <div>
-                        <h3 class="margin-details">Price</h3>
-                        <p class="margin-text">$15</p>
-                    </div>
-                </div>
-                <h3>Related dishes</h3>
-                <div class="imgs-related-container">
-                    <img class="img-related" src="./imgs/pork-chive.png" alt="pork-chive">
-                    <img class="img-related" src="./imgs/spring-rolls.png" alt="spring-rolls">
-                    <img class="img-related" src="./imgs/cucumber-salad.png" alt="cucumber-salad">
-                </div>
-            </div>
+
+            <?php
+            echo "<img class='img-dish' src='./imgs/" . $item[0]["dish_image"] . "' alt='" . $item[0]["dish_name"] . "'>";
+            echo "<div class='dish-information'>";
+                echo "<h2 class='dish-name'>" . $item[0]["dish_name"] . "</h2>";
+                echo "<p class='description'>" . $item[0]["dish_description"] . "</p>";
+                echo "<button class='star-btn'><img class='img-star' src='./imgs/empty-star.png' alt='empty-star'></button>";
+                echo "<div class='dish-caracteristics'>";
+                    echo "<div>";
+                    echo "<h3 class='margin-details'>Category</h3>";
+                    echo "<p class='margin-text'>".$item[0]["dish_category_name"]."</p>";
+                    echo "</div>";
+
+                    echo "<div class='divider'></div>";
+
+                    echo "<div>";
+                    echo "<h3 class='margin-details'>Dish for</h3>";
+                    echo "<p class='margin-text'>".$item[0]["category_people_description"]."</p>";
+                    echo "</div>";                    
+
+                    echo "<div class='divider'></div>";
+
+                    echo "<div>";
+                    echo "<h3 class='margin-details'>Price</h3>";
+                    echo "<p class='margin-text'>".$item[0]["dish_price"]."</p>";
+                    echo "</div>";  
+                echo "</div>";  
+
+                echo "<h3>Realeted Dishes</h3>";
+                echo "<div class='imgs-related-container'>";
+                    echo "<img class='img-related' src='./imgs/pork-chive.png' alt='pork-chive'>";
+                    echo "<img class='img-related' src='./imgs/spring-rolls.png' alt='spring-rolls'>";
+                    echo "<img class='img-related' src='./imgs/cucumber-salad.png' alt='cucumber-salad'>";
+                echo "</div>"; 
+            echo "</div>";  
+            ?>  
+             
         </div>
     </main>
     <footer class="footer">
